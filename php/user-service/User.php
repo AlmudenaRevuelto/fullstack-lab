4ce96a54748e05954bf578b62403deab5673ca18
Almudena Revuelto Quero <almudenarevuelto13@gmail.com>
2026-03-02 15:53:55 +0100

    Create User.php
    Add User model

---
+++ php/user-service/User.php
@@ -0,0 +1,28 @@
+<?php
+
+class User
+{
+    private string $name;
+    private int $age;
+
+    public function __construct(string $name, int $age)
+    {
+        $this->name = $name;
+        $this->age = $age;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getAge(): int
+    {
+        return $this->age;
+    }
+
+    public function increaseAge(): void
+    {
+        $this->age++;
+    }
+}
